a68985a348520675396445bce8b9daa718aa1854
James Buckingham <james@buckinghamdevelopment.co.uk>
2024-03-12 18:18:31 +0000

    Updates to the environment variable settings for production, following deployment.

--- Deployments/Phase 02/PSModules/Configuration.psm1
+++ Deployments/Phase 02/PSModules/Configuration.psm1
@@ -12,7 +12,7 @@
 #>
 
 # GLOBAL SETTINGS
-$global:environment = 'prod' # Which environment are we targetting?
+$global:environment = 'production' # Which environment are we targetting?
 
 # GLOBAL VARIABLES
 
